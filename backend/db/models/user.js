fbb7faf603d9f21bfc945ba3061d9a7a4cf41c88
chefvivica <vivicacongma@gmail.com>
2022-01-22 16:42:05 -0500

    save

---
+++ backend/db/models/user.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    username:{
+      type: DataTypes.STRING(100),
+      allowNull: false
+    },
+    winCount: {
+     type: DataTypes.INTEGER,
+     allowNull: false
+    },
+    lostCount:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
